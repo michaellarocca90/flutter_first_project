bb7b14ef004996d7c3d37880690dba5c5afce303
michaellarocca90 <33819929+michaellarocca90@users.noreply.github.com>
2019-03-21 19:47:36 -0700

    adds firebase packages and begins signin

---
+++ lib/pages/sign_in.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+class SignIn extends StatelessWidget{
+  @overrride
+    Widget(BuildContext context){
+
+    }
+}
